e778d940d89686651f6b00c6cd3310b8a4eeac50
Cole Patterson <cole.patter@gmail.com>
2024-08-03 17:09:25 -0500

    finalize project data

--- src/assets/projectsDesc.js
+++ src/assets/projectsDesc.js
@@ -8,15 +8,15 @@ export const projects = [
     //     description: `Have you ever been "Netflix and Chill"in with your partner and been overcome with the crushing existential dread of decision paralysis? Date Night looks to alleviate that anxiety by randomizing the selection of movie, and cocktail for the evening! By using The Movie Database API, and the Cocktail Database API, we here at 'Old Fashioned Industries' have created an application that will allow you to remove the responsibility of picking the perfect Movie and Drink to Chill with your partner/buddy/paramour. Did you forget what movie you watched after drinking many of the perfectly selected cocktails for your evening, DON'T WORRY! We have a history section where you can sort by movie or cocktail and see what you have previously selected! No more black out evenings without at least knowing what movie you picked..`,
     //     specification: ["HTML", "CSS", "JavaScript", "Web APIs", "Third-Party APIs"]
     // },
-    {
-        name: "Parkreation",
-        urlName: "parkreation",
-        thumbnail: "https://i.imgur.com/Y0LH4mP.gif",
-        repoUrl: "https://github.com/djubara/Parkreation",
-        deployedUrl: "",
-        description: "Parkreation is your ultimate companion for exploring national parks across the United States. Whether you're a seasoned traveler or a novice adventurer, Parkreation lets you discover, track, and share your national park experiences like never before.",
-        specification: ["JS", "HTML"]
-    },
+    // {
+    //     name: "Parkreation",
+    //     urlName: "parkreation",
+    //     thumbnail: "https://i.imgur.com/Y0LH4mP.gif",
+    //     repoUrl: "https://github.com/djubara/Parkreation",
+    //     deployedUrl: "",
+    //     description: "Parkreation is your ultimate companion for exploring national parks across the United States. Whether you're a seasoned traveler or a novice adventurer, Parkreation lets you discover, track, and share your national park experiences like never before.",
+    //     specification: ["JS", "HTML"]
+    // },
     {
         name: "Professional README Generator",
         urlName: "readme-gen",
@@ -29,10 +29,28 @@ export const projects = [
     {
         name: "Note Taker",
         urlName: "note-taker",
-        thumbnail: "",
+        thumbnail: "https://github.com/colepatters/uofm-fsw-challenge-20/blob/main/assets/challenge-11-thumb.png?raw=true",
         repoUrl: "https://github.com/colepatters/uofm-fsw-challenge-11",
         deployedUrl: "",
         description: "This web app allows you to take notes.",
-        specification: ["Express", "Node", ""]
+        specification: ["Express", "Node", "Server-side API"]
     },
+    {
+        name: "Weather Dashboard",
+        urlName: "weather-dashboard",
+        thumbnail: "https://github.com/colepatters/uofm-fsw-challenge-20/blob/main/assets/challenge-6-thumb.png?raw=true",
+        repoUrl: "https://github.com/colepatters/uofm-fsw-challenge-9",
+        deployedUrl: "",
+        description: "Why look out the window or ever go outside again when you can know what's going on out there with the best weather app you'll ever use!",
+        specification: ["HTML", "CSS", "CSS Library"]
+    },
+    // {
+    //     name: "",
+    //     urlName: "",
+    //     thumbnail: "https://github.com/colepatters/uofm-fsw-challenge-20/blob/main/assets/challenge-6-thumb.png?raw=true",
+    //     repoUrl: "https://github.com/colepatters/uofm-fsw-challenge-9",
+    //     deployedUrl: "",
+    //     description: "",
+    //     specification: ["HTML"]
+    // },
 ]
